b3c3717c092295d53777d9fe76e53abf9282b658
ValenGaymer <valenbustamanteq@gmail.com>
2024-05-16 22:04:40 -0500

    rererer

--- pages/modelp.py
+++ pages/modelp.py
@@ -5,6 +5,9 @@ from dash.dependencies import Input, Output, State
 
 dash.register_page(__name__)
 
+meses_tx = ['enero', 'febrero', 'marzo', 'abril', 
+         'mayo', 'junio', 'julio', 'agosto', 
+         'septiembre', 'octubre', 'noviembre', 'diciembre']
  
 layout = html.Div(
     [
@@ -81,7 +84,7 @@ layout = html.Div(
                         html.H4('Datos Ingresados'),
                         html.Ul(id='lista-datos')
                     ],
-                    style={'width': '50%', 'display': 'inline-block', 'verticalAlign': 'top', 'paddingLeft': '20px'}
+                    style={'width': '50%', 'display': 'inline-block', 'verticalAlign': 'top', 'padding-left': '20px'}
                 )
             ]
         ),
@@ -93,7 +96,7 @@ layout = html.Div(
             ]
         )
     ],
-    style={'padding': 5}
+    style={'padding-left': '20px'}
 )
 
 def register_callbacks(app):
@@ -111,17 +114,22 @@ def register_callbacks(app):
     def actualizar_iniciales(n_clicks, experimentados, entrenamiento, meses, tasa, expi):
         if n_clicks is None:
             return []
+        
+        if experimentados is None:
+            return[html.Li(f'Por favor, rellene todos los campos. ', 
+                    className = 'card border-warning mb-3', style = {'max-width': 380, 'text-align':'center', 'padding':10})]
+        
         datos = [
             html.Li(f' Pago de trabajadores experimentados: {experimentados}', 
-                    className = 'card border-primary mb-3', style = {'max-width': 380, 'text-align':'center', 'padding':10}),
+                    className = 'card border-warning mb-3', style = {'max-width': 380, 'text-align':'center', 'padding':10}),
             html.Li(f'Pago de trabajadores en entrenamiento: {entrenamiento}', 
-                    className = 'card border-primary mb-3', style = {'max-width': 380, 'text-align':'center', 'padding':10}),
+                    className = 'card text-white bg-warning mb-3', style = {'max-width': 380, 'text-align':'center', 'padding':10}),
             html.Li(f'Número de meses: {meses}', 
-                    className = 'card border-primary mb-3', style = {'max-width': 380, 'text-align':'center', 'padding':10}),
+                    className = 'card border-warning mb-3', style = {'max-width': 380, 'text-align':'center', 'padding':10}),
             html.Li(f'Tasa de abandono: {tasa}', 
-                    className = 'card border-primary mb-3', style = {'max-width': 380, 'text-align':'center', 'padding':10}),
+                    className = 'card text-white bg-warning mb-3', style = {'max-width': 380, 'text-align':'center', 'padding':10}),
             html.Li(f'Trabajadores experimentados iniciales: {expi}', 
-                    className = 'card border-primary mb-3', style = {'max-width': 380, 'text-align':'center', 'padding':10})
+                    className = 'card border-warning mb-3', style = {'max-width': 380, 'text-align':'center', 'padding':10})
         ]
 
         datamodel = [experimentados, entrenamiento, meses, tasa, expi]
@@ -135,18 +143,31 @@ def register_callbacks(app):
     def generar_inputs_rest(n_clicks, meses):
         if meses is None or meses <= 0:
             return []
+        
+        if meses > 12:
+            return html.H6('Meses inválidos. Ingrese un valor entre 1 y 12. ', className = 'badge rounded-pill bg-warning')
 
         inputs = []
         for i in range(meses):
             inputs.append(
                 html.Div(
                     [
-                        html.H6(f'Mes {i + 1}: ', className='form-label mt-4'),
+                        html.H6(f'Restricción de horas necesarias para el mes {i + 1}: ', className='form-label mt-4'),
                         dcc.Input(
                             id=f'input-mes-{i + 1}',
-                            type='number',
+                            type='text',
                             placeholder=f'Ingrese datos para el mes {i + 1}',
                             className='input-group-text'
+                        ),
+                        dcc.Dropdown(
+                            id=f'dropdown-mes-{i + 1}',
+                            options=[
+                                {'label': 'Mínimas', 'value': 'minimas'},
+                                {'label': 'Máximas', 'value': 'maximas'}
+                            ],
+                            placeholder='Restricción',
+                            className='input-group-text',
+                            style = {'max-width': 218.4}
                         )
                     ],
                     style={'marginBottom': '10px'}
@@ -154,4 +175,5 @@ def register_callbacks(app):
             )
         
         return inputs
+
     
